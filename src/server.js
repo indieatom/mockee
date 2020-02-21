0fa42a8e63f7659d2e09266f1631e9de66658ac2
Jordan Rocha <70rd23@gmail.com>
2020-02-21 14:04:04 -0300

    Instanciate server

---
+++ src/server.js
@@ -0,0 +1,18 @@
+module.exports = (
+    port = 4004,
+    mock = './mocks/mock.json',
+    ip = 'localhost'
+) => {
+    const resolve = require('path').resolve
+    const http = require('http')
+    const mockConfig = require(resolve(mock))
+    const mockHandler = require('./lib/mockHandler')
+
+    const server = http.createServer()
+
+    server.on('request', (req, res) => mockHandler(req, res, mockConfig))
+
+    server.listen(port, ip, () => {
+        console.log(`Mock server running! http://${ip}:${port}`)
+    })
+}
